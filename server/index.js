0887c417d08ab713fe4ab671da5827db60490864
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-12-08 17:56:43 +0100

    Swagger documentation for API setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const swaggerJsdoc = require("swagger-jsdoc");
+const dotenv = require("dotenv");
+const swaggerUi = require("swagger-ui-express");
+const cookieParser = require("cookie-parser");
+const { swaggerOptions } = require("./utils/constants.js");
+
+dotenv.config();
+const app = express();
+
+app.use(cookieParser());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(express.json());
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+const specs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.listen(process.env.PORT, () => {
+  console.log(`API-SERVER >> Server running on port ${process.env.PORT}`);
+});
---
+++ server/utils/constants.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Teller API Documentation",
+      version: "0.1.0",
+      description:
+        "This is a web application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Teller",
+        url: "https://teller.com",
+        email: "info@teller.com",
+      },
+    },
+    servers: [
+      {
+        url: process.env.BACKEND_URL,
+      },
+    ],
+  },
+  apis: ["./routes/*.js", "./controllers/*.js"],
+};
+
+module.exports = { swaggerOptions };
