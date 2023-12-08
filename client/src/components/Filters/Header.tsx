cd8859f3f9c5172ed46694774a9621d6228d5d08
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2023-12-09 00:20:57 +0100

    BackButton & Header components

---
+++ client/src/components/Filters/Header.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+interface HeaderProps {
+  genre: boolean;
+  ageRange: boolean;
+  prompt: boolean;
+}
+
+const Header = ({ genre, ageRange, prompt }: HeaderProps) => {
+  return (
+    <div className="relative grid grid-cols-1 text-center gap-y-8 sm:gap-y-10 md:gap-y-12 md:grid-cols-3 gap-x-12">
+      <div>
+        <div
+          className={`flex items-center justify-center w-16 h-16 mx-auto ${
+            genre
+              ? "bg-primaryColor border-2 border-primaryColor"
+              : "bg-white dark:bg-gray-800 border-2 border-gray-200 dark:border-gray-700"
+          } rounded-full shadow`}
+        >
+          <span className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+            1
+          </span>
+        </div>
+        <h3 className="mt-4 sm:mt-6 text-xl font-semibold leading-tight text-gray-900 dark:text-white md:mt-10">
+          Genre
+        </h3>
+        <p className="mt-3 sm:mt-4 text-base text-gray-600 dark:text-gray-400">
+          Choose genre for your story
+        </p>
+      </div>
+      <div>
+        <div
+          className={`flex items-center justify-center w-16 h-16 mx-auto ${
+            ageRange
+              ? "bg-primaryColor border-2 border-primaryColor"
+              : "bg-white dark:bg-gray-800 border-2 border-gray-200 dark:border-gray-700"
+          } rounded-full shadow`}
+        >
+          <span className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+            2
+          </span>
+        </div>
+        <h3 className="mt-4 sm:mt-6 text-xl font-semibold leading-tight text-gray-900 dark:text-white md:mt-10">
+          Age range
+        </h3>
+        <p className="mt-3 sm:mt-4 text-base text-gray-600 dark:text-gray-400">
+          Choose desired age range for your story.
+        </p>
+      </div>
+      <div>
+        <div
+          className={`flex items-center justify-center w-16 h-16 mx-auto ${
+            prompt
+              ? "bg-primaryColor border-2 border-primaryColor"
+              : "bg-white dark:bg-gray-800 border-2 border-gray-200 dark:border-gray-700"
+          } rounded-full shadow`}
+        >
+          <span className="text-xl font-semibold text-gray-700 dark:text-gray-200">
+            3
+          </span>
+        </div>
+        <h3 className="mt-4 sm:mt-6 text-xl font-semibold leading-tight text-gray-900 dark:text-white md:mt-10">
+          Prompt
+        </h3>
+        <p className="mt-3 sm:mt-4 text-base text-gray-600 dark:text-gray-400">
+          Insert text for your story.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
